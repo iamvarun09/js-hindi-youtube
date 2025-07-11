67b8d7b33cf4f47635ccfef8ef6f91655aea88f9
iamvarun09 <varundora0904@gmail.com>
2025-07-11 06:42:11 +0000

    datatypes02

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,13 @@
+
+console.log("")
+
+let accountname="varun"
+let accountpassword=9872
+let isloggedin=true
+let age=null;
+let undefined;
+
+console.log(typeof accountname)
+console.log(typeof isloggedin)
+console.log(typeof undefined)
+console.log(typeof null)
