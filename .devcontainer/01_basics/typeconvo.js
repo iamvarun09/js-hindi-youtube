7cb551fa2213a98ff107e6e9fd4d941d1a9ff1e6
iamvarun09 <varundora0904@gmail.com>
2025-07-11 07:17:53 +0000

    type conversion

---
+++ .devcontainer/01_basics/typeconvo.js
@@ -0,0 +1,18 @@
+
+let score=33
+console.log(typeof score)
+let stringinvalue=String(score)
+console.log(stringinvalue)
+console.log(typeof stringinvalue)
+
+let a=null
+console.log(typeof a)
+
+let valueinnumber=Number(a)
+console.log(typeof a)
+
+let b=undefined
+console.log(b)
+console.log(typeof b)
+let value_in_number=Number(b)
+console.log(typeof value_in_number)
