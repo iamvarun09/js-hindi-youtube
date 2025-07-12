44daeeebfe6659bde2d8cba877f3c81645809a5a
iamvarun09 <varundora0904@gmail.com>
2025-07-12 13:10:20 +0000

    datatypes2

---
+++ .devcontainer/01_basics/datatypes2.js
@@ -0,0 +1,31 @@
+const numbervalue=35;
+const numberinfloat=23.6
+const valueinstring="varun";
+const Emailid=undefined
+const isloggedin=true;
+const day=null;
+
+console.log(typeof numbervalue)
+console.log(typeof numberinfloat)
+console.log(typeof valueinstring)
+console.log(typeof Emailid)
+console.log(typeof isloggedin)
+console.log(typeof day)
+
+let arr1=["mango",1,6.8,"guava"]
+console.log(typeof arr1)
+
+let myObj={
+    name:"varundora",
+    emailid:"varundora0904@gmail.com",
+}
+console.log(typeof myObj)
+
+
+    
+const func=function(){
+
+        console.log("this is my function");
+
+        };
+console.log(typeof func)
