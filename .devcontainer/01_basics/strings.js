8e8f6cdc95adb6a9fe5a36c7c71abfd8539605f4
iamvarun09 <varundora0904@gmail.com>
2025-07-12 13:10:34 +0000

    strings.js

---
+++ .devcontainer/01_basics/strings.js
@@ -0,0 +1,10 @@
+
+const Mystring="varun-dora"
+console.log(Mystring.length)
+console.log(Mystring.toUpperCase())
+console.log(Mystring.charAt(5))
+console.log(Mystring.indexOf('n'))
+console.log(Mystring.includes("run"))
+console.log(Mystring.replace("varun","raghuvendra"))
+console.log(Mystring.substring(0,5))
+console.log(Mystring.split("-"))
