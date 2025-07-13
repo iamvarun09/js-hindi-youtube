29ec413ea27c10dddc2d0e3e4e0f13be3c3eab22
iamvarun09 <varundora0904@gmail.com>
2025-07-13 15:19:34 +0000

    date.js

---
+++ .devcontainer/01_basics/date.js
@@ -0,0 +1,21 @@
+
+let mydate=new Date()
+
+console.log(mydate)
+
+console.log(mydate.toLocaleString())
+console.log(mydate.toDateString())
+console.log(typeof mydate)
+
+console.log(mydate.now)
+
+console.log(mydate.getDay())
+
+console.log(mydate.getMonth())
+
+
+
+let mycreatedate=new Date(2025,0,14,20,46)
+console.log(mycreatedate.toLocaleString())
+
+
