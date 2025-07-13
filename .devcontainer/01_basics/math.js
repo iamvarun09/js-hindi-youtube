022f08daa314ff794e3196d152187f85ee12898b
iamvarun09 <varundora0904@gmail.com>
2025-07-13 03:59:31 +0000

    math and numbers

---
+++ .devcontainer/01_basics/math.js
@@ -0,0 +1,35 @@
+
+const Balance=new Number(100)
+console.log(Balance)
+
+
+let score=400
+console.log(score)
+
+
+console.log(Balance.toString().length)
+
+
+const otherNumber=23.678
+console.log(otherNumber.toPrecision(3))
+
+const hundreds=100000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+console.log(Math.sqrt(49))
+
+
+console.log(Math.abs(-9))
+
+console.log(Math.floor(7.9))
+
+console.log(Math.ceil(7.2))
+
+
+console.log(Math.min(4,3))
+
+
+console.log(Math.random())
+
+
