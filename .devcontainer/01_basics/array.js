b903b4c756359da6d11e2f29b6594132df37b5db
iamvarun09 <varundora0904@gmail.com>
2025-07-14 16:00:18 +0000

    array.js

---
+++ .devcontainer/01_basics/array.js
@@ -0,0 +1,54 @@
+
+
+const myarr=[0,1,2,3,4,5]
+
+console.log(myarr)
+
+
+console.log(myarr[0])
+
+const myarr2=new Array(1,2,3,4,5)
+
+console.log(myarr2)
+
+myarr2.push(6)
+
+console.log(myarr2)
+
+myarr2.push(7)
+
+console.log(myarr2)
+
+myarr2.pop()
+
+console.log(myarr2)
+
+myarr2.unshift(9)
+
+console.log(myarr2)
+
+myarr2.shift()
+
+console.log(myarr2)
+
+console.log(myarr2.includes(9))
+
+
+console.log(myarr2.indexOf(5))
+
+
+
+const newarr2=myarr2.join()
+
+console.log(myarr2)
+
+console.log(newarr2)
+
+
+console.log('A',myarr2)
+
+const newarr2_1=myarr2.slice(1,3)
+
+console.log(newarr2_1)
+
+console.log(myarr2)
