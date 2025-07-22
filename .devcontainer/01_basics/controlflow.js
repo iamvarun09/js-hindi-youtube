60f5d4ca9c9efb4e9e9d397507bd463ea52e3944
iamvarun09 <varundora0904@gmail.com>
2025-07-22 12:24:29 +0000

    controlflow

--- .devcontainer/01_basics/controlflow.js
+++ .devcontainer/01_basics/controlflow.js
@@ -106,6 +106,69 @@ do{
 
 }while(val!=10)
 
+//higher order array loops
+//for of
+  arr=["mango","litchee","guava","pineapple"]  
+  for(const key of arr){
+        console.log(key)
+  }
+
+  const map=new Map();
+
+  map.set("In",'india')
+  map.set("fr","france")
+  map.set("ger",'Gemany')
+
+  for(const i of map){
+    console.log(i);
+  }
+for(const [key,value] of map){
+    console.log(key,value)
+}
+    //for in
+const my_obj={
+    "name":"varundora",
+    "roll-no":"71",
+    "city":"bbsr"
+
+}
+for(let i in my_obj){
+    console.log(i)
+}
+
+for(let i in my_obj){
+   console.log(my_obj[i])
+}
+
+//for in  in case of array
+
+myarr2=[20,30,40,90]
+
+for(let key in myarr2){
+    console.log(key)
+}
+for(let key in myarr2){
+    console.log(myarr2[key])
+}
+
+//for each
+const coding=["ram","shyam","madhav","vasudev"]
+
+coding.forEach(function(item){                 //callback function no name
+     console.log(item)
+})
+
+coding.forEach((item)=>{
+   console.log(item)
+})
+
+const coding2=[13,15,19,21,35]
+coding2.forEach((i)=>{
+   if(i>15)
+    console.log(i)
+
+
+})
 
 
 
